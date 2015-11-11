50ac883d10e14de8e071219be92b0a68138f2932
Jeremy Magland <jeremy.magland@gmail.com>
2015-11-11 11:05:50 -0500

    window frame around main widget

--- mountainview/src/mountainviewwidget.cpp
+++ mountainview/src/mountainviewwidget.cpp
@@ -94,9 +94,10 @@ MountainViewWidget::MountainViewWidget(QWidget *parent) : QMainWindow(parent)
     CW->setLayout(VL);
 	this->setCentralWidget(CW);
 
-	this->setWindowFlags(Qt::WindowStaysOnTopHint);
-	this->setWindowFlags(Qt::X11BypassWindowManagerHint);
-	this->setWindowFlags(Qt::FramelessWindowHint);
+    this->setWindowFlags(Qt::WindowStaysOnTopHint);
+    this->setWindowFlags(Qt::CustomizeWindowHint | Qt::WindowTitleHint);
+    //this->setWindowFlags(Qt::X11BypassWindowManagerHint);
+    //this->setWindowFlags(Qt::FramelessWindowHint);
 }
 
 void MountainViewWidget::setElectrodeLocations(const Mda &L)
