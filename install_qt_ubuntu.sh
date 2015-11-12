ee79903d2e067dd8e3143113b92279cc8feaf313
Jeremy Magland <jeremy.magland@gmail.com>
2015-11-12 23:43:56 +0000

    add qt5-default to installs

--- install_qt_ubuntu.sh
+++ install_qt_ubuntu.sh
@@ -5,4 +5,4 @@ echo "Installing Qt5"
 sudo apt-add-repository ppa:ubuntu-sdk-team/ppa
 sudo apt-get update
 sudo apt-get install qtdeclarative5-dev
-sudo apt-get install qtbase5-dev qtscript5-dev make g++
+sudo apt-get install qt5-default qtbase5-dev qtscript5-dev make g++
