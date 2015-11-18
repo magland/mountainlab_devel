86dfd0dacf9b2ae3970af61cf06e7efc4df8ac74
Jeremy Magland <jeremy.magland@gmail.com>
2015-11-18 18:32:52 -0500

    templates

---
+++ processing/mscmd_templates.m
@@ -0,0 +1,15 @@
+function mscmd_templates(input_path,cluster_path,output_path,opts)
+
+if (nargin<4) opts=struct; end;
+
+cmd=sprintf('%s templates --input=%s --cluster=%s --output=%s --clip_size=%d ',mscmd_exe,input_path,cluster_path,output_path,opts.clip_size);
+
+fprintf('\n*** TEMPLATES ***\n');
+fprintf('%s\n',cmd);
+status=system(cmd);
+
+if (status~=0)
+    error('mountainsort returned with error status %d',status);
+end;
+
+end
