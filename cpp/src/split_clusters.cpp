4657304d74c027777698b70cb8b416d3c212741c
Jeremy Magland <jeremy.magland@gmail.com>
2015-11-21 04:08:14 -0500

    split clusters

--- cpp/src/split_clusters.cpp
+++ cpp/src/split_clusters.cpp
@@ -1,11 +1,14 @@
 #include "split_clusters.h"
 #include "diskreadmda.h"
 #include "mda.h"
+#include "get_principal_components.h"
+#include "isosplit.h"
 
 int get_K(DiskReadMda &C);
-void extract_clips(Mda &clips,DiskReadMda &X,QList<int> &times);
+void extract_clips(Mda &clips,DiskReadMda &X,QList<int> &times,int clip_size);
+void compute_features(Mda &features,Mda &clips,int num_features);
 
-bool cluster(const char *input_path,const char *cluster_path,const char *output_path,int num_features) {
+bool cluster(const char *input_path,const char *cluster_path,const char *output_path,int num_features,int clip_size) {
     DiskReadMda X; X.setPath(input_path);
     DiskReadMda C; C.setPath(cluster_path);
     Mda C2;
@@ -22,9 +25,9 @@ bool cluster(const char *input_path,const char *cluster_path,const char *output_
     for (int k=1; k<=K; k++) {
         QList<int> times=get_times(C,k);
         Mda clips;
-        extract_clips(clips,X,times);
+		extract_clips(clips,X,times,clip_size);
         Mda features;
-        compute_features(features,clips,num_features);
+		compute_features(features,clips,num_features);
         QVector<int> labels0=isosplit(features);
         int K0=get_max_k(labels0);
         for (aa=1; aa<=K0; aa++) {
@@ -49,6 +52,25 @@ int get_K(DiskReadMda &C) {
     return ret;
 }
 
-void extract_clips(Mda &clips,DiskReadMda &X,QList<int> &times) {
+void extract_clips(Mda &clips,DiskReadMda &X,QList<int> &times,int clip_size) {
+	int M=X.N1();
+	clips.allocate(M,clip_size,);
+	int tt1=-clip_size/2;
+	int tt2=tt1+clip_size-1;
+	for (int i=0; i<times.count(); i++) {
+		for (int t=0; t<clip_size; t++) {
+			for (int m=0; m<M; m++) {
+				clips.setValue(X.value(m,t0+t+tt1),m,t,i);
+			}
+		}
+	}
+}
 
+void compute_features(Mda &features,Mda &clips,int num_features) {
+
+	int M=clips.N1();
+	int T=clips.N2();
+	int num=clips.N3();
+	features.allocate(num_features,num);
+	get_principal_components(M*T,num,num_features,features.dataPtr(),clips.dataPtr());
 }
--- cpp/src/split_clusters.h
+++ cpp/src/split_clusters.h
@@ -1,7 +1,7 @@
 #ifndef SPLIT_CLUSTERS_H
 #define SPLIT_CLUSTERS_H
 
-bool cluster(const char *input_path,const char *cluster_path,const char *output_path,int num_features);
+bool cluster(const char *input_path,const char *cluster_path,const char *output_path,int num_features,int clip_size);
 
 #endif // SPLIT_CLUSTERS_H
 
