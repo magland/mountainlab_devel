f8658e100ae9af368c36d1f1661ea9d1c8dfe833
Jeremy Magland <magland@jm02.scdanet.org>
2016-02-13 21:56:28 -0500

    docs

--- processing/ms_templates.m
+++ processing/ms_templates.m
@@ -1,4 +1,24 @@
 function templates=ms_templates(clips,labels)
+%MS_TEMPLATES - Compute templates (mean waveforms) corresponding to each
+%spike type.
+%
+%Consider using mscmd_templates
+%
+% Syntax:  templates = ms_templates(clips,labels)
+%
+% Inputs:
+%    clips - MxTxNC array of clips
+%    labels - 1xNC array of integer labels
+%
+% Outputs:
+%    templates - MxTxK array of computed templates, K=max(labels)
+%
+% Other m-files required: none
+%
+% See also: mscmd_templates
+
+% Author: Jeremy Magland
+% Jan 2015; Last revision: 13-Feb-2106
 
 if (nargin<3) opts=struct; end;
 
--- processing/mscmd_features.m
+++ processing/mscmd_features.m
@@ -1,8 +1,33 @@
-function mscmd_features(input_path,detect_path,adjacency_path,output_path,opts)
+function mscmd_features(input_clips_path,detect_path,adjacency_path,output_path,opts)
+%MSCMD_FEATURES - Extract PCA features from an array of events
+%
+%This is a wrapper to the command-line mountainsort command. It has
+%similar functionality to ms_event_features.
+%
+% Syntax:  mscmd_features(input_clips_path,detect_path,adjacency_path,output_path,opts)
+%
+% Inputs:
+%    input_clips_path - path to MxTxNC array of clips (see mscmd_extract_clips)
+%    detect_path - 2xNC array output from mscmd_detect containing the
+%                  event channel numbers in the first row
+%    adjacency_path - path to the MxM adjacency matrix of 0's and 1's
+%                     defining which channels are adjacent to one another.
+%                     The PCA analysis will only be performed on a channel
+%                     and its adjacent neighbors.
+%    output_path - path to the (num_features x NC) array of features
+%    opts.num_features - the number of features to extract
+%    opts.clip_size - why is this parameter needed?
+%
+% Other m-files required: mscmd_exe
+%
+% See also: ms_event_features, mscmd_extract_clips, mscmd_detect
+
+% Author: Jeremy Magland and Alex Barnett
+% Oct 2015; Last revision: 13-Feb-2016
 
 if (nargin<4) opts=struct; end;
 
-cmd=sprintf('%s features --input=%s --detect=%s --adjacency=%s --output=%s ',mscmd_exe,input_path,detect_path,adjacency_path,output_path);
+cmd=sprintf('%s features --input=%s --detect=%s --adjacency=%s --output=%s ',mscmd_exe,input_clips_path,detect_path,adjacency_path,output_path);
 cmd=[cmd,sprintf('--num_features=%d --clip_size=%d ',opts.num_features,opts.clip_size)];
 
 fprintf('\n*** FEATURES ***\n');
