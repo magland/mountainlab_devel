a3a731cba12b23b557d9d47bdd222da8c5272b45
Jeremy Magland <jeremy.magland@gmail.com>
2016-02-16 18:25:06 -0500

    remove plotting in test_filter because crashing my laptop with too much RAM

--- unit_tests/test_filter.m
+++ unit_tests/test_filter.m
@@ -13,9 +13,9 @@ ofilt.freq_max=inf;
 mscmd_bandpass_filter(in,out,ofilt); Y = readmda(out);
 tic; Ym = ms_filter(X,ofilt); fprintf('ms matlab filter %.3g\n',toc)
 fprintf('rel output err btw matlab & C: %.3g\n',norm(Ym-Y)/norm(Y))
-Xh = fft(X); Yh = fft(Y);
-figure; subplot(2,1,1); t = (1:N)/samplerate; plot(t,X,'+-',t,Y,'o-');
-axis([T/2-.002 T/2+.002 -.5 1]); title('impulse response');
-subplot(2,1,2); plot(f(j),abs(Xh(:,j)).^2,'-');
-hold on; plot(f(j),abs(Yh(:,j)).^2,'r-');
-title('unfiltered and filtered |Xhat|'); drawnow
+%Xh = fft(X); Yh = fft(Y);
+%figure; subplot(2,1,1); t = (1:N)/samplerate; plot(t,X,'+-',t,Y,'o-');
+%axis([T/2-.002 T/2+.002 -.5 1]); title('impulse response');
+%subplot(2,1,2); plot(f(j),abs(Xh(:,j)).^2,'-');
+%hold on; plot(f(j),abs(Yh(:,j)).^2,'r-');
+%title('unfiltered and filtered |Xhat|'); drawnow
