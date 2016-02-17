cf92daec63c8cc34569f9099998ee6723a1152f5
ahbarnett <ahb@math.dartmouth.edu>
2016-02-17 11:18:12 -0500

    test_filter not a RAM hog; fixes #44

--- unit_tests/test_filter.m
+++ unit_tests/test_filter.m
@@ -1,11 +1,12 @@
 % test filter, Matlab vs C.  Barnett 2/16/16
 
 clear; in = [tempdir 'testfilt0.mda']; out = [tempdir 'testfilt1.mda'];
-M=8; N=1e7; X = zeros(M,N); X(:,N/2) = 1; %X = randn(M,N); % dummy data spike
+M=8; N=1e7;
+X = zeros(M,N); X(:,N/2) = 1;    % dummy data spike
+%X = randn(M,N);                 % white noise
 writemda(X,in);
 samplerate = 2e4;
 T = N/samplerate; df = 1/T; f=df*[0:N/2 -N/2+1:-1];
-j = 1:round(N/1e4):N/2+1;  % which freq indices to plot
 ofilt.samplefreq = samplerate;
 ofilt.freq_min=300;
 ofilt.freq_max=inf;
@@ -13,9 +14,12 @@ ofilt.freq_max=inf;
 mscmd_bandpass_filter(in,out,ofilt); Y = readmda(out);
 tic; Ym = ms_filter(X,ofilt); fprintf('ms matlab filter %.3g\n',toc)
 fprintf('rel output err btw matlab & C: %.3g\n',norm(Ym-Y)/norm(Y))
-%Xh = fft(X); Yh = fft(Y);
-%figure; subplot(2,1,1); t = (1:N)/samplerate; plot(t,X,'+-',t,Y,'o-');
-%axis([T/2-.002 T/2+.002 -.5 1]); title('impulse response');
-%subplot(2,1,2); plot(f(j),abs(Xh(:,j)).^2,'-');
-%hold on; plot(f(j),abs(Yh(:,j)).^2,'r-');
-%title('unfiltered and filtered |Xhat|'); drawnow
+figure; subplot(2,1,1); t = (1:N)/samplerate;
+trng = T/2 + [-1 1]*.002; i = (t>trng(1) & t<trng(2));  % t indices to plot
+plot(t(i),X(:,i),'+-',t(i),Y(:,i),'o-');
+axis([trng -.5 1]); title('impulse response');
+j = 1:round(N/1e4):N/2+1;                               % freq indices to plot
+Xh = fft(X(1,:)); Yh = fft(Y(1,:));                     % just check first row
+subplot(2,1,2); plot(f(j),abs(Xh(j)).^2,'-');
+hold on; plot(f(j),abs(Yh(j)).^2,'r-');
+title('unfiltered and filtered first row of |Xhat|'); drawnow
