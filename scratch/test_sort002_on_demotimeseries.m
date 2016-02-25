033408b2fb2fa0691aeb5db3a4e04f06bd8d3c3d
Jeremy Magland <jeremy.magland@gmail.com>
2016-02-24 19:05:41 -0500

    add

---
+++ scratch/test_sort002_on_demotimeseries.m
@@ -0,0 +1,48 @@
+function test_sort002_on_demotimeseries
+
+close all;
+
+%%%% Set up paths
+mfile_path=fileparts(mfilename('fullpath'));
+raw_path=[mfile_path,'/../unit_tests/demo_data/demotimeseries.mda'];
+if ~exist(raw_path), writedemotimeseries; end
+
+mfile_path=fileparts(mfilename('fullpath'));
+output_path=[mfile_path,'/output_sort001_on_demotimeseries'];
+if ~exist(output_path,'dir') mkdir(output_path); end;
+
+%%%% Sort
+sort_opts=struct;
+% sort_opts.shell.merge_threshold=0.5;
+% sort_opts.shell.section_increment=1;
+% sort_opts.shell.num_sections_per_shell=2;
+% sort_opts.shell.min_section_count=200;
+% sort_opts.detect.detect_interval=50;
+% sort_opts.detectibility_threshold=3;
+% sort_opts.isosplit.isocut_threshold=1.5;
+% sort_opts.isosplit.K_init=50;
+[firings_path,pre_path]=sort_002_multichannel(raw_path,output_path,sort_opts);
+
+%%%% View output
+mv.mode='overview2';
+mv.raw=pre_path;
+mv.firings=firings_path;
+ms_mountainview(mv);
+
+firings=readmda(firings_path);
+times=firings(2,:);
+labels=firings(3,:);
+raw=readmda(pre_path);
+clips=ms_extract_clips(raw,times,100);
+templates=ms_templates(clips,labels);
+figure; ms_view_templates(templates);
+for m=1:size(templates,1)
+    figure;
+    for k=1:size(templates,3)
+        plot(templates(m,:,k)); hold on;
+    end;
+    title(sprintf('channel %d',m));
+end;
+
+
+end
